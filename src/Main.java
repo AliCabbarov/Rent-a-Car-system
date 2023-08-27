5ce1578a9131dab722882d955b857b47d1a24ec6
AliCabbarov <ali.cab@div.edu.az>
2023-08-27 12:44:42 +0400

    added rent a car system basic

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import service.impl.ManagementServiceImpl;
+
+public class Main extends User {
+    public static void main(String[] args) {
+        new ManagementServiceImpl().manage();
+        User user = new User();
+
+
+    }
+}
